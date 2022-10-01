3971ac3430f433f9d84a46ab20322944e2b4e09b
Uno__sw <87971454+UnoCoding@users.noreply.github.com>
2022-10-01 15:16:43 +0900

    5_A

---
+++ Problems_macOS/5_A.cpp
@@ -0,0 +1,22 @@
+/*
+#include <iostream>
+#include <set>
+using  namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        string order1,order2;
+        cin>>order1>>order2;
+        set<char> s1;
+        set<char>s2;
+        for(int i=0;i<order1.size();i++)
+            s1.insert(order1[i]);
+        for(int i=0;i<order2.size();i++)
+            s2.insert(order2[i]);
+        if(s1==s2)
+            cout<<"YES"<<"\n";
+        else cout<<"NO"<<"\n";
+    }
+}
+*/
