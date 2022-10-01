1d3bbb7a0fa1c7cf90b8d1dc92211767b2fcdae4
Uno__sw <87971454+UnoCoding@users.noreply.github.com>
2022-10-01 14:56:57 +0900

    Initial Commit

---
+++ Problems_macOS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Problems_macOS
+//
+//  Created by Sung_1 on 2022/10/01.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
